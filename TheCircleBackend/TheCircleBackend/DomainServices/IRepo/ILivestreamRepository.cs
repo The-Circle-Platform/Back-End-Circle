6bcb7ba9ea429744adc3df5056fc3ada28e21e80
Thomas <vanotterloo.thomas@gmail.com>
2023-06-09 15:09:48 +0200

    Basis connectie SignalR

---
+++ TheCircleBackend/TheCircleBackend/DomainServices/IRepo/ILivestreamRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TheCircleBackend.Domain.Interfaces;
+using TheCircleBackend.Domain.Models;
+
+namespace TheCircleBackend.DomainServices.IRepo
+{
+	public class ILivestreamRepository
+	{
+		public ILivestreamRepository()
+		{
+            //public void AddStreamData(byte[] liveData);
+            //public void GetStreamVideo(int streamId);
+
+        }
+	}
+}
+
