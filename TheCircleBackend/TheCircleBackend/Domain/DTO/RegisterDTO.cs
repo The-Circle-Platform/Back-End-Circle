38a4d0e5ce5d5f1bd758e010f22aa34d92a541f9
Jascha <jascha3600@gmail.com>
2023-06-09 09:19:02 +0200

    JWT

---
+++ TheCircleBackend/TheCircleBackend/Domain/DTO/RegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheCircleBackend.Domain.DTO
+{
+    public class RegisterDTO
+    {
+        [Required (ErrorMessage = "Username is required")]
+        public string? Username { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+
+    }
+}
