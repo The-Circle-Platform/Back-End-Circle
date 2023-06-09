72cdb00a00183378ea3cc398b21b0a75f22c8c2d
Jascha <jascha3600@gmail.com>
2023-06-09 10:49:38 +0200

    JWT is back

--- TheCircleBackend/TheCircleBackend/Controllers/WeatherForecastController.cs
+++ TheCircleBackend/TheCircleBackend/Controllers/WeatherForecastController.cs
@@ -1,8 +1,14 @@
+//JWT authentication was implemented based on this tutorial: https://www.c-sharpcorner.com/article/jwt-authentication-and-authorization-in-net-6-0-with-identity-framework/
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TheCircleBackend.Domain.AuthModels;
+
 
 namespace TheCircleBackend.Controllers
 {
     [ApiController]
+    [Authorize(Roles = UserRoles.Admin)]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
