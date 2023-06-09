b7d59d55b0a4a93fb35325a22c2d0e2746f95abf
Jascha <jascha3600@gmail.com>
2023-06-09 12:02:50 +0200

    added tests

---
+++ TheCircleBackend/Tests/UserControllerTests/UserTests.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using TheCircleBackend.Controllers;
+using TheCircleBackend.DBInfra;
+using TheCircleBackend.DBInfra.Repo;
+using TheCircleBackend.Domain.Models;
+
+namespace Tests.UserControllerTests
+{
+   
+    public class UserTests
+    {
+        [Fact]
+        public void GetAllWebsiteUsersReturns2UsersIfTheDatabaseContainsTwoUsers()
+        {
+            //arrange
+            var dbContextOptions = new DbContextOptionsBuilder<DomainContext>().UseInMemoryDatabase("DomainTestDB").Options;
+            var domainContext = new DomainContext(dbContextOptions);
+            var ilogger = new Mock<ILogger<EFWebsiteUserRepo>>();
+            var sut = new EFWebsiteUserRepo(domainContext, ilogger.Object);
+            var testUser = new WebsiteUser()
+            {
+                Id = 1,
+                IsOnline = true,
+                UserName = "henk"
+            };
+            var testUser2 = new WebsiteUser()
+            {
+                Id = 2,
+                IsOnline = false,
+                UserName = "henk2"
+            };
+            domainContext.Add(testUser);
+            domainContext.Add(testUser2);
+            domainContext.SaveChanges();
+
+            //act
+            var result = sut.GetAllWebsiteUsers();
+            //assert
+            Assert.True(result.Count() == 2);
+
+            domainContext.Database.EnsureDeleted();
+        }
+
+        [Fact]
+        public void GetByIdIdReturnsUser1IfIdIs1AndUserWithIdOneExists()
+        {
+            //arrange
+            var dbContextOptions = new DbContextOptionsBuilder<DomainContext>().UseInMemoryDatabase("DomainTestDB").Options;
+            var domainContext = new DomainContext(dbContextOptions);
+            var ilogger = new Mock<ILogger<EFWebsiteUserRepo>>();
+            var sut = new EFWebsiteUserRepo(domainContext, ilogger.Object);
+            var testUser = new WebsiteUser()
+            {
+                Id = 1,
+                IsOnline = true,
+                UserName = "henk"
+            };
+            var testUser2 = new WebsiteUser()
+            {
+                Id = 2,
+                IsOnline = false,
+                UserName = "henk2"
+            };
+            domainContext.Add(testUser);
+            domainContext.Add(testUser2);
+            domainContext.SaveChanges();
+            //act
+            var result = sut.GetById(1);
+            //assert
+            Assert.True(result == testUser);
+            
+            domainContext.Database.EnsureDeleted();
+
+        }
+
+        [Fact]
+        public void GetByIdReturnsNullIfUserDoesNotExist()
+        {
+            //arrange
+            var dbContextOptions = new DbContextOptionsBuilder<DomainContext>().UseInMemoryDatabase("DomainTestDB").Options;
+            var domainContext = new DomainContext(dbContextOptions);
+            var ilogger = new Mock<ILogger<EFWebsiteUserRepo>>();
+            var sut = new EFWebsiteUserRepo(domainContext, ilogger.Object);
+            var testUser = new WebsiteUser()
+            {
+                Id = 1,
+                IsOnline = true,
+                UserName = "henk"
+            };
+            var testUser2 = new WebsiteUser()
+            {
+                Id = 2,
+                IsOnline = false,
+                UserName = "henk2"
+            };
+            domainContext.Add(testUser);
+            domainContext.Add(testUser2);
+            domainContext.SaveChanges();
+            //act
+            var result = sut.GetById(3);
+            //assert
+            Assert.Null(result);
+
+            domainContext.Database.EnsureDeleted();
+        }
+
+        [Fact]
+        public void AddAddsUser()
+        {
+            //arrange
+            var dbContextOptions = new DbContextOptionsBuilder<DomainContext>().UseInMemoryDatabase("DomainTestDB").Options;
+            var domainContext = new DomainContext(dbContextOptions);
+            var ilogger = new Mock<ILogger<EFWebsiteUserRepo>>();
+            var sut = new EFWebsiteUserRepo(domainContext, ilogger.Object);
+            var testUser = new WebsiteUser()
+            {
+                Id = 1,
+                IsOnline = true,
+                UserName = "henk"
+            };
+            //act
+            sut.Add(testUser);
+            //assert
+            Assert.Equal(1, domainContext.WebsiteUser.Count());
+            domainContext.Database.EnsureDeleted();
+
+        }
+
+        [Fact]
+        public void UpdateUpdatesUser()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<DomainContext>().UseInMemoryDatabase("DomainTestDB").Options;
+            var domainContext = new DomainContext(dbContextOptions);
+            var ilogger = new Mock<ILogger<EFWebsiteUserRepo>>();
+            var sut = new EFWebsiteUserRepo(domainContext, ilogger.Object);
+            var testUser = new WebsiteUser()
+            {
+                Id = 1,
+                IsOnline = true,
+                UserName = "henk"
+            };
+            domainContext.Add(testUser);
+            domainContext.SaveChanges();
+            testUser.UserName = "ingrid";
+            //act
+            sut.Update(testUser, 1);
+
+            //assert
+            var assertResult = domainContext.WebsiteUser.Where(u => u.Id == 1).First();
+            Assert.Equal("ingrid", assertResult.UserName);
+            domainContext.Database.EnsureDeleted();
+
+
+
+
+        }
+    }
+}
