b53226383f6343b1bab568b43eba5cea7fa1863c
mhaddouc1 <m.haddouch1@student.avans.nl>
2023-06-15 15:28:10 +0200

    Image backend gemaakt

---
+++ TheCircleBackend/TheCircleBackend/Migrations/20230615130735_editProfilePicToAddedUser.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheCircleBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class editProfilePicToAddedUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Base64Image",
+                table: "WebsiteUser",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "WebsiteUser",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Base64Image",
+                table: "WebsiteUser");
+
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "WebsiteUser");
+        }
+    }
+}
