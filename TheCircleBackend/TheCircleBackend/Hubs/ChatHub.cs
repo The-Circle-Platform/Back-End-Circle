81da7bf61554731955c161b88ff17f12d1fc674f
xin <xin20wang@outlook.com>
2023-06-19 15:01:18 +0200

    Fixed infinite recursion

--- TheCircleBackend/TheCircleBackend/Hubs/ChatHub.cs
+++ TheCircleBackend/TheCircleBackend/Hubs/ChatHub.cs
@@ -32,66 +32,93 @@ namespace TheCircleBackend.Hubs
 
             // Generate server keypair
             var ServerKeyPair = security.GetServerKeys();
-
-            // Creates hash and creates signature, based on this hash.
-            var signedData = security.SignData(list, ServerKeyPair.privKey);
-
-            // Creates DTO to send to client.
-            var ChatMessageDTO = new ChatListDTOOutcoming()
+            // Converts code
+            var NewUpdatedList = new List<ChatMessageLite2>();
+            foreach (ChatMessage message in list)
             {
-                OriginalList = list,
-/*                PublicKey = ServerKeyPair.pubKey,*/
-                Signature = signedData
-            };
-
-            //Send back to client.
-            await Clients.All.SendAsync($"ReceiveChat-{receiverUserId}", ChatMessageDTO);
-        }
-        
-        // Receiver method
-        public async Task SendMessage(ChatMessageDTOIncoming incomingChatMessage)
-        {
-            // RetrieveUserKeys
-            var publicKeyUser = security.GetKeys(incomingChatMessage.SenderUserId).pubKey;
-
-            // Checks if integrity is held.
-            bool HeldIntegrity = security.HoldsIntegrity(incomingChatMessage.OriginalData, incomingChatMessage.Signature, publicKeyUser);
-
-            if (HeldIntegrity)
-            {
-                throw new ArgumentException("Message has been tampered. Try later");
+                NewUpdatedList.Add(new ChatMessageLite2()
+                {
+                    receiverUser = message.ReceiverUser.UserName,
+                    writer = message.Writer.UserName,
+                    date = message.Date,
+                    message = message.Message,
+                    receiverId = message.ReceiverId,
+                    webUserId = message.WebUserId
+                });
             }
-            else
-            {
-                // Persisteer in database. Tabel chats (StreamId, UserId, DatumTijd en Content)
-
-                ChatMessage chatMessage = incomingChatMessage.OriginalData;
-
-                //Inserts chat message
-                messageRepository.Create(chatMessage);
-
-                // Lees geupdate versie
-                var updatedList = messageRepository.GetStreamChat(incomingChatMessage.OriginalData.ReceiverId);
-
-                // Generate server keypair
-                var ServerKeyPair = security.GetServerKeys();
-
                 // Creates hash and creates signature, based on this hash.
-                var signedData = security.SignData(updatedList, ServerKeyPair.privKey);
+                var signedData = security.SignData(NewUpdatedList, ServerKeyPair.privKey);
 
                 // Creates DTO to send to client.
-                var OutcomingMessage = new ChatListDTOOutcoming()
+                var ChatMessageDTO = new ChatListDTOOutcoming()
                 {
-                    Signature = signedData,
-                    OriginalList = updatedList,
-                    SenderUserId = incomingChatMessage.SenderUserId
+                    OriginalList = NewUpdatedList,
+                    /*                PublicKey = ServerKeyPair.pubKey,*/
+                    Signature = signedData
                 };
 
-                // Send new data to client.
-                await Clients.All.SendAsync($"ReceiveChat-{incomingChatMessage.OriginalData.ReceiverId}",
-                    OutcomingMessage);
+                //Send back to client.
+                await Clients.All.SendAsync($"ReceiveChat-{receiverUserId}", ChatMessageDTO);
             }
-        }
+
+            // Receiver method
+            public async Task SendMessage(ChatMessageDTOIncoming incomingChatMessage)
+            {
+                var incom = incomingChatMessage;
+                // RetrieveUserKeys
+                var publicKeyUser = security.GetKeys(incomingChatMessage.SenderUserId).pubKey;
+
+                // Checks if integrity is held.
+                bool HeldIntegrity = security.HoldsIntegrity(incomingChatMessage.OriginalData, incomingChatMessage.Signature, publicKeyUser);
+
+                if (HeldIntegrity)
+                {
+                    throw new ArgumentException("Message has been tampered. Try later");
+                }
+                else
+                {
+                    // Persisteer in database. Tabel chats (StreamId, UserId, DatumTijd en Content)
+
+                    ChatMessage chatMessage = incomingChatMessage.OriginalData;
+
+                    //Inserts chat message
+                    messageRepository.Create(chatMessage);
+
+                    // Lees geupdate versie
+                    var updatedList = messageRepository.GetStreamChat(incomingChatMessage.OriginalData.ReceiverId);
+                    // Converts code
+                    var NewUpdatedList = new List<ChatMessageLite2>();
+                    foreach (ChatMessage message in updatedList)
+                    {
+                        NewUpdatedList.Add(new ChatMessageLite2()
+                        {
+                            receiverUser = message.ReceiverUser.UserName,
+                            writer = message.Writer.UserName,
+                            date = message.Date,
+                            message = message.Message,
+                            receiverId = message.ReceiverId,
+                            webUserId = message.WebUserId
+                        });
+                    }
+                    // Generate server keypair
+                    var ServerKeyPair = security.GetServerKeys();
+
+                    // Creates hash and creates signature, based on this hash.
+                    var signedData = security.SignData(NewUpdatedList, ServerKeyPair.privKey);
+
+                    // Creates DTO to send to client.
+                    var OutcomingMessage = new ChatListDTOOutcoming()
+                    {
+                        Signature = signedData,
+                        OriginalList = NewUpdatedList,
+                        SenderUserId = incomingChatMessage.SenderUserId
+                    };
+
+                    // Send new data to client.
+                    await Clients.All.SendAsync($"ReceiveChat-{incomingChatMessage.OriginalData.ReceiverId}",
+                        OutcomingMessage);
+                }
+            } 
 
         public override Task OnConnectedAsync()
         {
